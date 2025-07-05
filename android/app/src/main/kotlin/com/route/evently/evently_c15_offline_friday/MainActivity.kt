509f23916ae0a53c75f6bc2c2d319f0044678ba5
muhammedSaadX1 <muhammedsaad.x1@gmail.com>
2025-07-05 11:03:51 +0300

    initialize project

---
+++ android/app/src/main/kotlin/com/route/evently/evently_c15_offline_friday/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.route.evently.evently_c15_offline_friday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EventlyApp());
+}
+
+class EventlyApp extends StatelessWidget {
+  const EventlyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp();
+  }
+}
+
